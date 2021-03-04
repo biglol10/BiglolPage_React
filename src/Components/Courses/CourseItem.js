10b05787d34c497d4f230c9fc38a1ff31a2e1f00
biiglol <46738034+biiglol@users.noreply.github.com>
2021-03-04 17:31:40 +0900

    courses taken page updated

--- src/Components/Courses/CourseItem.js
+++ src/Components/Courses/CourseItem.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import './CourseItem.css';
+import RatingComp from '../CommonComp/RatingComp';
 
-function CourseItem() {
+function CourseItem({name, url, path, instructor_details, price, rating}) {
     return (
-        <div>
-            
+        <div className="courseItem">
+            <a className="courseImage" href={url}>
+                <div className="courseImage playButtonTrigger">
+                    <img src={path} alt="" style={{width:'230px', height:'135px'}}/>
+                    <div className="playButton"></div>
+                </div>
+            </a>
+            <a className="courseExplanation" href={url}>
+                <div className="courseDetails">
+                    <strong>{name}</strong>
+                    <div className="courseInstructor">
+                        <span>{instructor_details.length > 50 ? instructor_details.substring(0,50) + '...' : instructor_details}</span>
+                    </div>
+                    <div className="coursePricingRating">
+                        <div><span>&#8361; {price}</span></div>
+                        <div><RatingComp rating={rating}/></div>
+                        <br/>
+                    </div>
+                </div>
+            </a>
         </div>
     )
 }
--- src/Components/Courses/Courses.js
+++ src/Components/Courses/Courses.js
@@ -1,17 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import './Courses.css';
+import CourseItem from './CourseItem';
+import axios from '../../axios';
 
 function Courses() {
-    const buttonChange = (idValue) => {
+    const buttonChange = (idValue, type) => {
         const buttonContainerClass = document.getElementsByClassName('courseCategory');
         for (let idx = 0; idx < buttonContainerClass.length; idx++) {
             buttonContainerClass[idx].classList.remove('categoryShow');
         }
         const buttonContainerId = document.getElementById(idValue);
         buttonContainerId.classList.add('categoryShow');
+        setCourseType(type);
     }
-    
 
+    const [courseType, setCourseType] = useState('T');
+    const [courseList, setCourseList] = useState([]);
+
+    useEffect(()=>{
+        const getCourseList = async () => {
+            const response = await axios({
+                method: 'get',
+                url: `/courses/${courseType}`
+            })
+            setCourseList(response.data)
+        }
+        getCourseList();
+    }, [courseType])
+    
+    console.log(courseList);
     return (
         <div className="coursesPage">
             <div className="courseText">
@@ -19,20 +36,31 @@ function Courses() {
             </div>
             <div className="courseContainer">
                 <div className="courseButtons">
-                    <div onClick={() => buttonChange('courseT')} id="courseT" className="courseCategory categoryShow">
+                    <div onClick={() => buttonChange('courseT', 'T')} id="courseT" className="courseCategory categoryShow">
                         <span>
                             Courses Taken
                         </span>
                     </div>
-                    <div onClick={() => buttonChange('courseI')} id="courseI" className="courseCategory">
+                    <div onClick={() => buttonChange('courseI', 'I')} id="courseI" className="courseCategory">
                         <span>
                             Courses Interested
                         </span>
                     </div>
                 </div>
                 <hr/>
-                <div className="courseDetails">
-                    
+                <div className="courseWrapper">
+                    {
+                        courseList.map((item, i) => (
+                            <CourseItem
+                                name={item.name}
+                                url={item.url}
+                                path={item.path}
+                                instructor_details={item.instructorDetails}
+                                price={item.price}
+                                rating={item.rating}
+                            />
+                        ))
+                    }
                 </div>
             </div>
             
